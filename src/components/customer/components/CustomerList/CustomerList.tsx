b7767d499b4f3317942aaa1d42af6ab6167531f1
pranita <kalepranita96@gmail.com>
2024-12-11 14:46:55 +0530

    initiate data integration - fetch api response

--- src/components/customer/components/CustomerList/CustomerList.tsx
+++ src/components/customer/components/CustomerList/CustomerList.tsx
@@ -1,5 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 const CustomerList = () => {
-	return <div>CustomerList</div>;
+	const [userDetails, setUserDetails] = useState(null);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const response = await axios.get('https://dummyjson.com/users');
+			console.log(response.data);
+
+			setUserDetails(response.data);
+		};
+
+		fetchUsers();
+	}, []);
+	return (
+		<div>
+			{userDetails ? (
+				<div>Display details</div>
+			) : (
+				<div>No User Data to display.</div>
+			)}
+		</div>
+	);
 };
 
 export default CustomerList;
