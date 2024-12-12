c062d09b8bcca9e833be35b72329104caecc62ff
pranita <kalepranita96@gmail.com>
2024-12-12 11:01:15 +0530

    display user data in table

--- src/components/customer/components/CustomerList/CustomerList.tsx
+++ src/components/customer/components/CustomerList/CustomerList.tsx
@@ -2,22 +2,51 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const CustomerList = () => {
-	const [userDetails, setUserDetails] = useState(null);
+	const [userDetails, setUserDetails] = useState([]);
+
+	const fetchUsers = async () => {
+		const response = await axios.get('https://dummyjson.com/users');
+		console.log(response.data.users);
+
+		setUserDetails(response.data.users);
+	};
+
+	const formatDate = (dateString: any) => {
+		if (!dateString) return '';
+		const [year, month, day] = dateString.split('-');
+		return `${day}-${month}-${year}`;
+	};
 
 	useEffect(() => {
-		const fetchUsers = async () => {
-			const response = await axios.get('https://dummyjson.com/users');
-			console.log(response.data);
-
-			setUserDetails(response.data);
-		};
-
 		fetchUsers();
 	}, []);
 	return (
 		<div>
-			{userDetails ? (
-				<div>Display details</div>
+			{userDetails && userDetails.length > 0 ? (
+				<div>
+					<table>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>City</th>
+								<th>Birth Date</th>
+							</tr>
+						</thead>
+						<tbody>
+							{userDetails.map((user: any, index: number) => (
+								<tr key={index}>
+									<td>
+										{[user.firstName, user.lastName].join(
+											' '
+										)}
+									</td>
+									<td>{user.address.city}</td>
+									<td>{formatDate(user.birthDate)}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
 			) : (
 				<div>No User Data to display.</div>
 			)}
